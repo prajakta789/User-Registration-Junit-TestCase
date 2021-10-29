03797ff40c23cc23e5476d3aae9aa321d08eda3c
prajakta <prajaktamahajan789@gmail.com>
2021-10-29 18:24:16 +0530

    Added:User need to Enter Valid Email Address

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -2,24 +2,35 @@ package com.bridgelabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 	
-	public class UserRegistration {
-	    /*
-	    Created a method to validate first name
-	     */
-	    public boolean firstName(String firstName) {
-	        String regex = "^[A-Z]{1}[a-z]{2,}$";
-	        Pattern pattern = Pattern.compile(regex);
-	        Matcher matcher = pattern.matcher(firstName);
-	        return matcher.matches();
-	    }
+public class UserRegistration {
 
-	    /*
-	    Created a method to validate last name
-	     */
-	    public boolean lastName(String lastName) {
-	        String regex = "^[A-Z]{1}[a-z]{2,}$";
-	        Pattern pattern = Pattern.compile(regex);
-	        Matcher matcher = pattern.matcher(lastName);
-	        return matcher.matches();
-	    }
-	}
+    /*
+    Created a method to validate first name
+     */
+    public boolean firstName(String firstName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
+    /*
+    Created a method to validate last name
+     */
+    public boolean lastName(String lastName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
+    /*
+    Created a method to validate E-mail
+     */
+    public boolean email(String email) {
+        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
